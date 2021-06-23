87ca02ba46134407e0a117ef57076c2396c13b50
Patrick N. Kuria <kuria144@gmail.com>
2021-06-23 11:08:21 +0300

    Heroku commit

--- core/settings/base.py
+++ core/settings/base.py
@@ -92,8 +92,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kurzsystems',
+        'USERNAME': 'postgres',
+        'PASSWORD': 'system123',
     }
 }
 
